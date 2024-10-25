649c0b7357388b3342e4aeaaee2c8c5306b6ac9e
CsCheems <jmanuelm94@outlook.com>
2024-10-24 21:04:03 -0600

    Creacion de layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import {DatePicker} from 'antd';
 import {ConfigProvider} from 'antd';
-import './styles/variables.css';
-import './App.css';
+import LayoutComponent from './Layouts/LayoutComponent';
+import './styles/variables.css'
+import './App.css'
+import ImageLogin from './Components/ImageLogin';
+import FormLogin from './Components/FormLogin';
 
 function App() {
 
@@ -13,7 +16,12 @@ function App() {
           colorPrimary: '#9F81F7'
         }
       }}>
-      <DatePicker />
+      <LayoutComponent
+        leftColSize={{xs:0, sm:0, md:20, lg:18}}
+        rightColSize={{xs:24, sm:24, md:4, lg:6}}
+        leftContent={<ImageLogin/>}
+        rightContent={<FormLogin/>}
+      />
     </ConfigProvider>
       
     </>
---
+++ src/Components/ImageLogin/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const ImageLogin = () => {
+    return (
+        <h1>Image Login</h1>
+    );
+}
+
+export default ImageLogin;
